8fa487ffdd551869ca5ab7d3030a1a8113ebbc19
Taras Terletskyi <888784+tropicoo@users.noreply.github.com>
2022-10-15 18:27:27 +0300

    Limit filename to 200 bytes

--- worker/ytdl_opts/default.py
+++ worker/ytdl_opts/default.py
@@ -4,7 +4,7 @@ from core.config import settings
 
 # More here https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/options.py
 YTDL_OPTS = {
-    'outtmpl': os.path.join(settings.TMP_DOWNLOAD_PATH, '%(title)s.%(ext)s'),
+    'outtmpl': os.path.join(settings.TMP_DOWNLOAD_PATH, '%(title).200B.%(ext)s'),
     'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
     'noplaylist': True,
     'max_downloads': 1,

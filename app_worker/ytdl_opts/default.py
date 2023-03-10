2bc673ce9ec576eaf0d93f160ff68d80dba10e8f
Taras Terletskyi <888784+tropicoo@users.noreply.github.com>
2023-03-11 01:13:24 +0200

    Update docstrings

--- app_worker/ytdl_opts/default.py
+++ app_worker/ytdl_opts/default.py
@@ -1,7 +1,10 @@
-"""yt-dlp download CLI options.
+"""Default `yt-dlp` download CLI options.
 
 Only CLI options are allowed to be stored as configuration. They are later converted to internal API options.
 More here https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/options.py or 'yt-dlp --help'
+
+If you want to change any of these values or add new ones, copy all content to the `user.py` in the same
+directory as this file, and edit the values.
 """
 
 FINAL_AUDIO_FORMAT = 'mp3'
---
+++ app_worker/ytdl_opts/user.py
@@ -0,0 +1,7 @@
+"""User-specific `yt-dlp` download CLI options.
+
+Only CLI options are allowed to be stored as configuration. They are later converted to internal API options.
+More here https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/options.py or 'yt-dlp --help'
+
+Copy all content from the `default.py` here and edit values.
+"""

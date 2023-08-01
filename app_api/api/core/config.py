d4297ec82a08a0bd810584c1d3b375fb65ffeb97
Taras Terletskyi <888784+tropicoo@users.noreply.github.com>
2023-08-02 00:52:09 +0300

    Add api workers config value

--- app_api/api/core/config.py
+++ app_api/api/core/config.py
@@ -4,6 +4,7 @@ from yt_shared.config import Settings
 class ApiSettings(Settings):
     API_HOST: str
     API_PORT: int
+    API_WORKERS: int
 
 
 settings = ApiSettings()
--- app_api/main.py
+++ app_api/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 
-from api.core.app import app  # noqa: F401
+from api.core.app import app
 from api.core.config import settings
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host=settings.API_HOST, port=settings.API_PORT)
+    uvicorn.run(
+        app,
+        host=settings.API_HOST,
+        port=settings.API_PORT,
+        workers=settings.API_WORKERS,
+    )

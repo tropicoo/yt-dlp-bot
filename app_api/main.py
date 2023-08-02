4777855e2962365f2479ee7c0c366efbe64416ad
Taras Terletskyi <888784+tropicoo@users.noreply.github.com>
2023-08-02 23:53:49 +0300

    Add api workers config value

--- app_api/main.py
+++ app_api/main.py
@@ -1,11 +1,11 @@
 import uvicorn
 
-from api.core.app import app
+from api.core.app import app  # noqa: F401
 from api.core.config import settings
 
 if __name__ == '__main__':
     uvicorn.run(
-        app,
+        'main:app',
         host=settings.API_HOST,
         port=settings.API_PORT,
         workers=settings.API_WORKERS,
